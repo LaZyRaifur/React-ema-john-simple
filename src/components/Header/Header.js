5f2911329f6423f62b03f2ad51c12e10ac2278fd
Raifurrahim <raifurrahimcse13@gmail.com>
2020-05-07 22:41:12 +0600

    shopo page coding done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from'../../images/logo.png';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className="Header">
+                <img src={logo}></img>
+                <nav>
+            <a href="/shop">Shop</a>
+            <a href="/review">Order Review</a>
+            <a href="/manage">Manage Ineventory</a>
+          </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props.product);
+    const { img, name, seller, price, stock } = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button className="main-button" 
+                onClick={() => props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Product;     
